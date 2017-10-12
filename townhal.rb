88d99851f73ca93a52adf1eec13f9cb8b5e15671
Etienne Bouron <etienne.toutbien@gmail.com>
2017-10-12 16:28:07 +0200

    with unavailable option

--- townhal.rb
+++ townhal.rb
@@ -12,15 +12,23 @@ def get_the_email_of_a_townhal_from_its_webpage
     city_links.each do |link|   
     city_array << link['href'].sub!(".", "http://annuaire-des-mairies.com")
 end
-    	#puts city_array
-	townhall_email_array = []
+    	
+    townhall_email_array = []
 	city_array.each do |url|
 	page = Nokogiri::HTML(open("#{url}"))
+	error = []
 
 		content_array = page.css("td[class=style27]").text.split
 	    content_array.each do |adress|
-	    	townhall_email_array << adress if adress.include?("@")
-	 end
+	    	if adress.include?("@")
+	    		townhall_email_array << adress
+	    		error << adress 
+	    	end
+	    end
+
+	if error.size == 0
+	 	townhall_email_array << "Unavailable"
+	end
 end
 	puts townhall_email_array
 	puts townhall_email_array.size
