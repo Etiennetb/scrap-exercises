66d77b02419cb73e2ba9aa40c93281ef6f40412a
Etienne Bouron <etienne.toutbien@gmail.com>
2017-10-12 18:47:57 +0200

    trading complet with hash and timer

--- trading.rb
+++ trading.rb
@@ -1,3 +1,5 @@
+#!/usr/bin/ruby
+
 require "rubygems"
 require "nokogiri"
 require "open-uri"
@@ -5,6 +7,27 @@ require "pry"
 
 
  def get_currency_name
+ loop {
+ 	page = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/"))
+		
+
+    trader = {}
+    name_array = page.css("a[class='price']")
+    name_array.each do |info|   
+    	currency = info['href'].sub!("/currencies/", "").sub!("/#markets", "")
+		value = info['data-usd']
+
+		trader[currency] = value
+	end
+
+ 	puts pp trader
+sleep(3600)}
+ end
+ 
+
+
+
+def get_currency_name_old
  	page = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/"))
 		puts page.class
 
@@ -23,7 +46,5 @@ require "pry"
  puts price_array
 
  end
- 
-
 
 get_currency_name
